b304d2c907b8bef7e28b46e3045f3af44643445a
Alex White <a24white@edu.uwaterloo.ca>
2019-05-17 22:26:11 +0000

    Don't use import for server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
-import express from 'express';
-import path from 'path';
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../dist')));
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, process.env.IP, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
